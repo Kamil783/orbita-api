38fb197a40ae4a89e686a132f8f7b25e0c45effe
Kamil <galyamov.kamil5@gmail.com>
2026-02-22 17:50:37 +0300

    fix

--- Orbita.Infrastructure/Persistence/SeedDatabase.cs
+++ Orbita.Infrastructure/Persistence/SeedDatabase.cs
@@ -51,7 +51,6 @@ public static class SeedDatabase
             {
                 UserName = email,
                 Email = email,
-                UserName = email,
                 EmailConfirmed = true
             };
 
