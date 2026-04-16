4335620cdcc8853a155fba5452d0f92d60d0c580
Kamil <galyamov.kamil5@gmail.com>
2026-04-16 15:35:35 +0300

    Add migration

---
+++ Orbita.Infrastructure/Persistence/Migrations/20260416123517_AddTeamCapacity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orbita.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamCapacity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeamCapacities",
+                columns: table => new
+                {
+                    TeamId = table.Column<Guid>(type: "uuid", nullable: false),
+                    WeekdayHours = table.Column<int>(type: "integer", nullable: false),
+                    WeekendHours = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamCapacities", x => x.TeamId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamCapacities");
+        }
+    }
+}
